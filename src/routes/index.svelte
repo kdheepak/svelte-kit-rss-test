9c52f447b38e5b680513bcc81fd1f495a01c15fc
Dheepak Krishnamurthy <me@kdheepak.com>
2022-02-17 01:14:43 -0700

    chore: Update files ♻️

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,13 @@
-<script>
+<script context="module">
   import { base } from '$app/paths'
+</script>
+
+<script>
+  export let humanDate
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <a href="{base}/link">go to link</a>
+
+<p>{humanDate}</p>
