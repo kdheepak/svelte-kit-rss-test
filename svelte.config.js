4c317d4d2d3e8d352a015375cf26aa72da307807
Dheepak Krishnamurthy <me@kdheepak.com>
2022-02-17 00:01:39 -0700

    chore: Update files ♻️

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
 const isProduction = process.env.NODE_ENV == 'production' ? true : false
-const productionBaseDirectory = 'svelte-kit-rss-test'
+const productionBaseDirectory = 'svelte-kit-test'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
